1a319fe8fa7f906195a150470291ce37ccebe025
YoungHwan Heo <bethestar12@gmail.com>
2022-03-02 16:58:31 +0900

    [feat] init class interface
    Signed-off-by: YoungHwan Heo <bethestar12@gmail.com>

---
+++ FineCode/DB_interface.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+#include <vector>
+
+#include "common.h"
+
+using namespace std;
+
+class IDB {
+public:
+    virtual int add(Employee& employee) = 0;
+    virtual vector<Employee>& retrieve(enum Column, string tobe) = 0;
+    virtual int remove(vector<int> employeeNums) = 0; // 삭제된 사원 수 반환. (음수 에러)​
+    virtual int modify(vector<int> employeeNums, enum Column, string tobe) = 0;// 수정된 사원 수 반환(음수 에러)
+};
---
+++ FineCode/common.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+#include <string>
+using namespace std;
+enum class EInfoType {
+    employeeNum,
+    name,
+    cl,
+    phoneNum,
+    birthday,
+    certi,
+};
+
+typedef struct Employee {
+    int employeeNum;
+    string name;
+    string cl​;
+    string phoneNum​;
+    string birthday​;
+    string certi​;
+}​;
