027d6917b4ab8fe6c9cc3d64f97370fbf38dd3dd
wolframhwang <hzw94@naver.com>
2022-03-06 02:07:10 +0900

    [feat] IDB 기능 추가
    IDB에 Search, Erase, Modify 기능을 추가했습니다.
    1. Condition을 통해 조건에 부합하는 Condition을 추출할수 있습니다.
    2. Erase를 통해 Numbering한 요소를 지울수 있습니다.
    3. Modify를 통해 대상을 변경 가능합니다.

--- FineCode/IDataBase.h
+++ FineCode/IDataBase.h
@@ -1,20 +1,23 @@
 #pragma once
 #include <map>
+#include <vector>
 #include "Employee.h"
 #include "cmd.h"
+#include "Condition.h"
 
 using namespace std;
 
 struct IDataBase {
     virtual bool add(EmployeeInfo employeeInfo) = 0;
-    virtual bool modify(CmdModify &command) = 0;
+    virtual bool modify(unsigned long employeeNumber, Condition &modifyCondtion) = 0;
     virtual bool erase(unsigned int employeeNumber) = 0;
+    virtual vector<unsigned long> search(Condition &searchCondition) = 0;
     virtual EmployeeInfo* getEmployeeInfo(unsigned int employeeNumber) = 0;
 };
 
 class DataBase: public IDataBase {
 public:
-    DataBase() {}
+    DataBase() { mutex = false; }
     virtual ~DataBase() {}
 
     virtual bool add(EmployeeInfo employee) override {
@@ -22,17 +25,30 @@ public:
         return employeesList.insert(make_pair(employeeNum, employee)).second;
     }    
     
-    virtual bool modify(CmdModify &command) override {
-        for (auto employee: employeesList) {
-            
-        }
-        if(employeesList.find(employeeNumber) == employeesList.end()) return false;
-        employeesList[employeeNumber] = employeeInfo;
+    virtual bool modify(unsigned long employeeNumber, Condition &modifyCondition) override {
+        if(mutex) return false;
+        mutex = true;
+        modifyCondition.set(employeesList[employeeNumber]);
+        mutex = false;
         return true;
     }
 
     virtual bool erase(unsigned int employeeNumber) override {
+        if(mutex) return false;
+        mutex = true;
+        employeesList.erase(employeeNumber);
+        mutex = false;
         return employeesList.erase(employeeNumber);
+    }
+
+    virtual vector<unsigned long> search(Condition &searchCondition) {
+        vector<unsigned long> ret;
+        for(auto employee: employeesList) {
+            if (searchCondition.isEqual(employee.second)) {
+                ret.push_back(employee.first);
+            }
+        }
+        return ret;
     }
 
     virtual EmployeeInfo* getEmployeeInfo(unsigned int employeeNumber) override {
@@ -41,6 +57,6 @@ public:
     }
     
 private:
-    map<int, EmployeeInfo> employeesList;
-
+    map<unsigned long, EmployeeInfo> employeesList;
+    bool mutex;
 };
