a8a3fb5c5fa5ba9672af1d20625da806da8b514e
Sanghun Han <sahsh61ng@gmail.com>
2022-03-07 17:09:18 +0900

    [feat] Employee의 toString 기능 구현

--- FineCode/Employee.h
+++ FineCode/Employee.h
@@ -44,7 +44,51 @@ struct Employee {
     }
 
     string toString(void) const {
-        // TODO: 구현 필요
-        return "";
+        string result;
+        result += toStringEmployeeNum() + ",";
+        result += name.first + " " + name.last + ",";
+        result += toStringCl() + ",";
+        result += "010-" + toStringNum(phoneNum.mid, 4) + "-" + toStringNum(phoneNum.end, 4) + ",";
+        result += toStringNum(birthday.year, 4) + toStringNum(birthday.month, 2) + toStringNum(birthday.day, 2) + ",";
+        result += toStringCerti();
+        return result;
+    }
+
+private:
+    string toStringEmployeeNum(void) const {
+        auto result = to_string(employeeNum);
+        return string(result.c_str() + 2, 8);
+    }
+
+    string toStringCl(void) const {
+        switch (cl) {
+        case CL::CL1:
+            return "CL1";
+        case CL::CL2:
+            return "CL2";
+        case CL::CL3:
+            return "CL3";
+        case CL::CL4:
+            return "CL4";
+        }
+    }
+
+    string toStringCerti(void) const {
+        switch (certi) {
+        case Grade::ADV:
+            return "ADV";
+        case Grade::PRO:
+            return "PRO";
+        case Grade::EX:
+            return "EX";
+        }
+    }
+
+    string toStringNum(int num, int length) const {
+        auto result = to_string(num);
+        while (result.length() < length) {
+            result = "0" + result;
+        }
+        return result;
     }
 };
