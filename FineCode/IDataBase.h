ee4b7a65c91c027e96617be593dc0aea8ae7a81d
Sanghun Han <sahsh61ng@gmail.com>
2022-03-07 17:24:30 +0900

    [feat] Database 구현 추가

--- FineCode/IDataBase.h
+++ FineCode/IDataBase.h
@@ -1,62 +1,52 @@
 #pragma once
-#include <map>
+#include <list>
 #include <vector>
 #include "Employee.h"
 #include "Condition.h"
+#include "Result.h"
 
 using namespace std;
 
 struct IDataBase {
-    virtual bool add(Employee employeeInfo) = 0;
-    virtual bool modify(unsigned long employeeNumber, Condition &modifyCondtion) = 0;
-    virtual bool erase(unsigned int employeeNumber) = 0;
-    virtual vector<unsigned long> search(Condition &searchCondition) = 0;
-    virtual Employee* getEmployee(unsigned int employeeNumber) = 0;
+    virtual void add(Employee &employee, Result &result) = 0;
+    virtual void modify(const Condition &targetCondition, const Condition &modifyCondition, Result &result) = 0;
+    virtual void erase(const Condition &targetCondition, Result &result) = 0;
+    virtual void search(const Condition &targetCondition, Result &result) = 0;
 };
 
 class DataBase: public IDataBase {
 public:
-    DataBase() { mutex = false; }
-    virtual ~DataBase() {}
-
-    virtual bool add(Employee employee) override {
-        int employeeNum = employee.employeeNum;
-        return employeesList.insert(make_pair(employeeNum, employee)).second;
+    virtual void add(Employee &employee, Result &result) override {
+        result.insert(employee);
+        employees_.push_back(employee);
     }    
     
-    virtual bool modify(unsigned long employeeNumber, Condition &modifyCondition) override {
-        if(mutex) return false;
-        mutex = true;
-        modifyCondition.set(employeesList[employeeNumber]);
-        mutex = false;
-        return true;
-    }
-
-    virtual bool erase(unsigned int employeeNumber) override {
-        if(mutex) return false;
-        if(employeesList.find(employeeNumber) == employeesList.end()) return false;
-        mutex = true;
-        employeesList.erase(employeeNumber);
-        mutex = false;
-        return employeesList.erase(employeeNumber);
-    }
-
-    virtual vector<unsigned long> search(Condition &searchCondition) {
-        vector<unsigned long> ret;
-        for(auto employee: employeesList) {
-            if (searchCondition.isEqual(employee.second)) {
-                ret.push_back(employee.first);
+    virtual void modify(const Condition &targetCondition, const Condition &modifyCondition, Result &result) override {
+        for (auto employee : employees_) {
+            if (targetCondition.isEqual(employee)) {
+                result.insert(employee);
+                modifyCondition.set(employee);
             }
         }
-        return ret;
     }
 
-    virtual Employee* getEmployee(unsigned int employeeNumber) override {
-        if (employeesList.find(employeeNumber) == employeesList.end()) return NULL;
-        return &employeesList[employeeNumber];
+    virtual void erase(const Condition &targetCondition, Result &result) override {
+        for (auto employee : employees_) {
+            if (targetCondition.isEqual(employee)) {
+                result.insert(employee);
+                employees_.remove(employee);
+            }
+        }
+    }
+
+    virtual void search(const Condition &targetCondition, Result &result) override {
+        for (auto employee : employees_) {
+            if (targetCondition.isEqual(employee)) {
+                result.insert(employee);
+            }
+        }
     }
     
 private:
-    map<unsigned long, Employee> employeesList;
-    bool mutex;
+    list<Employee> employees_;
 };
