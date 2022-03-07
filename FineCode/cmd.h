1a393069acacb32aff80eca4a3d30ee11c89d56d
YoungHwan Heo <bethestar12@gmail.com>
2022-03-07 12:19:27 +0900

    [test] modify cmd class test cases
       Todo : get proper condition instance
    
    Signed-off-by: YoungHwan Heo <bethestar12@gmail.com>

--- FineCode/cmd.h
+++ FineCode/cmd.h
@@ -3,12 +3,12 @@
 #include "IDataBase.h"
 #include "Employee.h"
 
-struct ICmd {
+class ICmd {
 public:
     static shared_ptr<ICmd> getCmd(string CmdType); // ADD, DEL, SCH, MOD
     virtual bool execute(const shared_ptr<IDataBase> database) = 0;
     virtual const vector<string>& getParsedCmd() const = 0;
-    virtual bool setParsedCmd(const vector<string>& parsedCmd)  = 0;
+    virtual void setParsedCmd(const vector<string>& parsedCmd)  = 0;
     virtual void setEmployee(const shared_ptr<Employee> employee) = 0;
     virtual void setCondition(const shared_ptr<Condition> targetCondition) = 0;
 };
