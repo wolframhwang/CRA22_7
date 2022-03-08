c4a4d746b8e6103ea1fb0a6be598044fddb01ba3
nodtrio <nodtrio16@naver.com>
2022-03-08 11:42:51 +0900

    [refactor] 필요없는 출력 및 주석 삭제
    cmdParser called!!!, <CMD> 삭제건입니다.

--- FineCode/InputManager.h
+++ FineCode/InputManager.h
@@ -44,7 +44,6 @@ public:
         }
 
         getline(inputFileStream_, line);
-        cout << "cmdParser called!!!   " + line << endl;
 
         vector<string> values;
         string::size_type Fpos = line.find_first_not_of(',', 0);
@@ -83,7 +82,6 @@ private:
     vector<string> parsedCmd_;
 
     void setEmployeeNum(shared_ptr<Employee> employee) {
-        // REMIND: Need to append "19" or "20" for easy sorting.
         employee->employeeNum = stoi(parsedCmd_.at(INPUT_EMPLOYEENUM));
     }
 
